cb88066abb78a58688183821f7bc26efb2785042
Vishal Rathnam <vishalrathamspr@gmail.com>
2018-06-03 19:26:44 -0500

    added model api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/',(req,res)=>{
   res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+app.get('/users',(req,res)=>{
+  res.json([
+    {"id":1,"firstName":"John","lastName":"Doe","email":"johndoe@gmail.com"},
+    {"id":2,"firstName":"Jane","lastName":"Doe","email":"janedoe@gmail.com"},
+    {"id":3,"firstName":"Ezio","lastName":"Auditore","email":"ezioauditore@gmail.com"}
+  ]);
+});
+
 app.listen(port,(err)=>{
   if(err)
   {
